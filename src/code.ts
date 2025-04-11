b22afc8fc58c8555ef1c75cd9825773228b19c36
Alex Harris <alexharris6@gmail.com>
2025-04-11 10:45:16 -0700

    glitch it

---
+++ src/code.ts
@@ -0,0 +1,46 @@
+// Import functions for generating a palette from an image
+import * as utils from "./modules/utils";
+
+// Turn on the UI
+figma.showUI(__html__, { themeColors: true, height: 600, width: 400 })
+
+
+function selectImage() {
+  var selectedImage = utils.getSelectedImage();
+
+  selectedImage.then((value) => {
+    if (value !== undefined) {
+      // Send the image to the UI
+      figma.ui.postMessage({ type: 'show-image-in-ui', imageDetails: value });
+    } else {
+      figma.ui.postMessage({ type: 'no-image-selected'});
+    }
+  });
+}
+// get the currently selected image on load
+selectImage();
+  
+  // get the currently selected image on selection change
+figma.on('selectionchange', () => {
+  selectImage();
+})
+
+
+// ---------------
+//  RECEIVE MESSAGES
+// ---------------
+
+// Check for a message from the UI
+figma.ui.onmessage = msg => {
+
+ 
+}
+
+
+
+
+
+
+
+
+
---
+++ src/modules/utils.ts
@@ -0,0 +1,35 @@
+// Get Selected Image
+// All this does is figure out if/which image is selected
+
+export async function getSelectedImage() {
+  // console.log('get selected image')
+  // Get the selected nodes
+  const selectedNodes = figma.currentPage.selection;
+  // Check if a node is selected
+  if (selectedNodes.length > 0) {
+    const node = selectedNodes[0];
+    // Check if the node is an image
+    if ('fills' in node) {
+      const fills = node.fills as Paint[];
+      // Check if the fill type is IMAGE
+      if (fills[0].type === 'IMAGE') {
+        
+        // Get the image hash
+        const imageHash = fills[0].imageHash;
+        // Get the image
+        const image = imageHash ? figma.getImageByHash(imageHash) : null;
+        
+        if (image) {
+          // get image bytes from getBytesAsync
+          // const size = await image.getSizeAsync();
+          const bytes = await image.getBytesAsync();
+          // This returns a promise to code.ts
+          
+          return { imageBytes: bytes, height: node.height, width: node.width };
+        } else {
+          console.log('no image selected. error needed')
+        }
+      }
+    }
+  }
+}
